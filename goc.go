53c0c137d9d9bb02265fe6ec468856914b75510c
darth-218 <yahia.hany.gaber@proton.me>
2024-06-18 15:35:31 +0300

    Better prompt, better file creation/deletion, started addin lines to files

--- goc.go
+++ goc.go
@@ -4,29 +4,48 @@ import (
   "os"
   "bufio"
   "strings"
+  "io"
 )
 
 func main() {
   goc()
 }
 
-func goc() {
-  var status int = 1
+func goc() { 
+  var status int = 0
+  var files = []string{"./current.c", "./history.c"}
+  source_files := createFiles(files)
+  source_current, source_history := source_files[0], source_files[1]
   reader := bufio.NewReader(os.Stdin)
-
-  os.Create("./history.c")
-  os.Create("./current.c")
-
-  for status != -1 {
-    line, err := reader.ReadString('\n')
+  for status != 1 {
+    fmt.Printf(">> "); line, err := reader.ReadString('\n')
     if err == nil {
       line = strings.TrimSuffix(line, "\n")
-      fmt.Println(line)
+      addLine(source_current, source_history, line)
+      status = 0
     } else {
-      fmt.Println(err)
-      continue
+      fmt.Printf("\n%s, exiting...\n", err)
+      status = 1
     }
   }
-  os.Remove("./history.c")
-  os.Remove("./current.c")
+  removeFiles(files)
+}
+
+func addLine(current, history io.Writer, line string) {
+  fmt.Fprintln(current, line)
+  fmt.Fprintln(history, line)
+}
+
+func removeFiles(files []string) {
+  for _, file := range files {
+    os.Remove(file)
+  } 
+}
+
+func createFiles(files []string) (source_files []*os.File){
+  for _, file := range files {
+    source_file, _ := os.Create(file)
+    source_files = append(source_files, source_file)
+  }
+  return
 }
